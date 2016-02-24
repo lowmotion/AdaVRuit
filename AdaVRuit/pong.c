6c050254f5f9be794749b7d464bd332934f3bfa1
lowmotion <marcojedzig@gmail.com>
2016-02-24 19:33:53 +0100

    Tobis Pong und Kommentare
    Tobis Pong wurde von mir in mein Programmgerüst eingefügt und teilweise
    überarbeitet. Zudem habe ich einen einheitlichen Kommentarheader für
    Funktionen gemacht und die Definitionen reingeschrieben. 
    In calcBallPosition() habe ich im Fall eines Tors (Goal() wird
    aufgerufen) noch einen return eingefügt, damit calcBallPosition() an der
    Stelle beendet wird.
    
    
    Signed-off-by: lowmotion <marcojedzig@gmail.com>

--- AdaVRuit/pong.c
+++ AdaVRuit/pong.c
@@ -14,36 +14,33 @@
 #include "pong.h"
 
 
-#define ROWS 8
-#define COLUMNS 16
-#define BAT_WIDTH 3
-
-
-/*	Struktur: Ball
- * 	Beschreibung: Diese Struktur stellt den Spielball dar.
+/*	Struktur: 		Ball
+ * 	Beschreibung: 	Diese Struktur stellt den Spielball dar.
  */
 struct Ball {
 	uint8_t posX, posY, prevX, prevY;
 };
 
-/*	Struktur: Player (Spieler)
- * 	Beschreibung: Diese Struktur stellt einen Spieler dar.
+/*	Struktur: 		Player (Spieler)
+ * 	Beschreibung: 	Diese Struktur stellt einen Spieler dar.
  */
 struct Player {
-	uint8_t posX, posY, prevX, prevY, width;
+	uint8_t posX, posY, prevX, prevY, width, goals;
 };
 
+/* Globale Variablen */
+
+struct Ball ball;
+struct Player playerL, playerR;
 
 
-/** Funktion: initPong
- * 	Beschreibung: Diese Funktion initialisiert das Spiel Pong. Es werden zwei Spieler erstellt
- * 		und auf dem Spielfeld positioniert. Jeweils links und rechts oben in den Ecken. Außerdem
- * 		wird der Spielball so positioniert, dass der linke Spieler anfängt. (Zeile 4 Spalte 8)
+/* Funktion: 		initPong
+ * Beschreibung: 	Diese Funktion initialisiert das Spiel Pong. Es werden zwei Spieler initialisiert
+ * 					und auf dem Spielfeld positioniert. Jeweils links und rechts oben in den Ecken.
+ * 					Außerdem wird der Spielball so positioniert, dass der linke Spieler anfängt. (Zeile 4 Spalte 8)
  */
 void initPong() {
-	struct Ball ball = { 4, 8};
-	struct Player player1 = {0, 0, BAT_WIDTH};
-	struct Player player2 = {0, 7, BAT_WIDTH};
+
 }
 
 /*
@@ -60,21 +57,86 @@ void inputUserControls() {
 
 }
 
-/*	Funktion: Timer Interrupt Service Routine
- * 	Beschreibung: Der Timer zählt eine gewisse Zeit X hoch. Wenn der Interrupt ??? auftritt wird
- * 		diese Funktion aufgerufen, die die neue Position von dem Spielball berechnet.
+/*	Funktion: 		Timer Interrupt Service Routine
+ * 	Beschreibung: 	Der Timer zählt eine gewisse Zeit X hoch. Wenn der Interrupt ??? auftritt wird
+ * 					diese Funktion aufgerufen, die die neue Position von dem Spielball berechnet.
  *
  */
 ISR(TIMER0_COMP_vect) {
 	calcBallPosition();
 }
 
-/*
+/* Funktion:		calcBallPosition
+ * Beschreibung:	Berechnet die Position des Balls neu. Hierbei werden die derzeitige Bewegungsrichtung
+ * 					des Balls, Wände und Schläger der Spieler berücksichtigt. Falls der Ball eine der
+ * 					Seitenwände erreicht, wird die Funktion Goal() aufgerufen, da ein Tor gefallen ist.
  *
  */
 void calcBallPosition() {
+	// Derzeitige Bewegungsrichtung
+	uint8_t movX, movY;				// kein unsigned, da +/-
+	movX = ball.posX - ball.prevX;
+	movY = ball.posY - ball.prevY;
 
-}
+	// Überschreiben der alten Positionswerte
+	ball.prevX = ball.posX;
+	ball.prevY = ball.posY;
+
+
+	// Testen des linken Randes
+	if(ball.prevX == FIRST_COLUMN+1){
+		if(playerL.posY == ball.prevY - 1) {movX = +1; movY = +1;}
+		else if(playerL.posY == ball.prevY) {movX = +1; movY = 0;}
+		else if(playerL.posY == ball.prevY + 1) {movX = +1; movY = -1;}
+		else {
+			Goal(playerR);
+			return;		// Beendet die Funktion calcBallPosition, da in Goal() neu initialisiert wird
+		}
+	}
+
+	// Testen des rechten Randes
+	if(ball.prevX == LAST_COLUMN-1){
+		if(playerR.posY == ball.prevY - 1) {movX = -1; movY = +1;}
+		else if(playerR.posY == ball.prevY) {movX = -1; movY = 0;}
+		else if(playerR.posY == ball.prevY + 1) {movX = -1; movY = -1;}
+		else {
+			Goal(playerL);
+			return;		// Beendet die Funktion calcBallPosition, da in Goal() neu initialisiert wird
+		}
+	}
+
+	/*	ALLE Eckpunkte werden Abgedeckt, da dort die R�nder dominierend sind und movY im folgenden �berschreiben k�nnen. */
+
+	// Testen des oberen Randes
+	if(ball.prevY == FIRST_LINE){
+		movY = +1;
+	}
+
+	// Testen des unteren Randes
+	if(ball.prevY == LAST_LINE){
+		movY = -1;
+	}
+
+	// Berechnung der neuen Position
+	ball.posX = ball.prevX + movX;
+	ball.posY = ball.prevY + movY;
+};
+
+/*
+ * Funktion:		Goal
+ * Beschreibung:	Diese Funktion incrementiert den Spielstand (Variable goal im struct)
+ * 					des übergebenen Spielers. Der Ball wird zurückgesetzt und die vorherige
+ * 					Position wird so gesetzt, dass Ball sich als erstes wieder in die Richtung
+ * 					des Spielers bewegt, der keinen Punkt gemacht hat. Somit Aufschlag hat.
+ */
+void Goal(struct Player player){
+	// Punkt z�hlen f�r Spieler player
+
+	// verr�ckte Ausgabe machen, dass ein Punkt gefallen ist
+
+	// Spielball neu Initialisieren
+
+};
 
 
 void playPong() {
--- AdaVRuit/pong.h
+++ AdaVRuit/pong.h
@@ -10,4 +10,15 @@
 
 #include "main.h"
 
+/* Konstanten */
+#define ROWS 		8	// Anzahl der Zeilen des Spielfelds in Pixeln
+#define COLUMNS 	16	// Anzahl der Spalten ders Spielfelds in Pixeln
+#define BAT_WIDTH 	3	// Breite der Schläger der Spieler in Pixeln
+
+#define FIRST_LINE		0			// Index der ersten Zeile
+#define LAST_LINE		ROWS - 1	// Index der letzten Zeile
+#define FIRST_COLUMN	0			// Index der ersten Spalte
+#define LAST_COLUMN		COLUMNS - 1	// Index der letzten Spalte
+
+
 #endif /* PONG_H_ */

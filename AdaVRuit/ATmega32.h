92e147a963ed32aa0c18a605455b6c6fbd08c7a1
lowmotion <marcojedzig@gmail.com>
2016-02-26 15:29:03 +0100

    Öffentliche Funktionen
    Habe alle Funktionen aus ATmega32.c, die andere Module verwenden können
    sollen in der Header Datei definiert. Damit sind sie z. B. auch in
    pong.c aufrufbar.

--- AdaVRuit/ATmega32.h
+++ AdaVRuit/ATmega32.h
@@ -16,4 +16,32 @@ void TWI_sendByte(uint8_t ui_adress, uint8_t ui_data);
 
 void TWI_sendArray(uint8_t ui_data[8][2]);
 
+uint8_t ui_player1_L();
+uint8_t ui_player1_R();
+uint8_t ui_player1_U();
+uint8_t ui_player1_D();
+uint8_t ui_player2_L();
+uint8_t ui_player2_R();
+uint8_t ui_player2_U();
+uint8_t ui_player2_D();
+
+uint8_t ui_player1_L(uint8_t _ui_buttons);
+uint8_t ui_player1_R(uint8_t _ui_buttons);
+uint8_t ui_player1_U(uint8_t _ui_buttons);
+uint8_t ui_player1_D(uint8_t _ui_buttons);
+uint8_t ui_player2_L(uint8_t _ui_buttons);
+uint8_t ui_player2_R(uint8_t _ui_buttons);
+uint8_t ui_player2_U(uint8_t _ui_buttons);
+uint8_t ui_player2_D(uint8_t _ui_buttons);
+
+
+void resetPlayer1_L(uint8_t *_ui_buttons);
+void resetPlayer1_R(uint8_t *_ui_buttons);
+void resetPlayer1_U(uint8_t *_ui_buttons);
+void resetPlayer1_D(uint8_t *_ui_buttons);
+void resetPlayer2_L(uint8_t *_ui_buttons);
+void resetPlayer2_R(uint8_t *_ui_buttons);
+void resetPlayer2_U(uint8_t *_ui_buttons);
+void resetPlayer2_D(uint8_t *_ui_buttons);
+
 #endif /* ATMEGA32_H_ */

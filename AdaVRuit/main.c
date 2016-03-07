7479d544a737d490bac5d561dda95e7cdc0eecd9
tobmag <tobias.mages@gmx.de>
2016-03-07 10:02:57 +0100

    Abänderung der Kommentare nach Vorgabe

--- AdaVRuit/main.c
+++ AdaVRuit/main.c
@@ -1,9 +1,20 @@
-/*
- * main.c
- *
- *  Created on: Feb 11, 2016
- *      Author: marco
- */
+ /*****************************************************************************
+*
+* FILE: 	 	main.c
+* PROJECT:	      	Spielekonsole
+* MODULE:		???
+*
+* Description:		Hauptprogramm
+*
+* Notes:		-
+*
+* Compiler dependencies or special instructions:
+*
+* REVISION HISTORY
+* Date:   11.02.2016
+* BY:	  Michel, Marco, Michael, Christian, Tobias
+*
+*****************************************************************************/
 
 #include "main.h"
 
@@ -15,14 +26,30 @@
  */
 uint8_t matrix[8][2];
 
-/*
- * Funktion:			printBit
- * Beschreibung:		Ändert ein bestimmtes Bit in der LED Matrix.
- * 						Adressierung:
- * 							Zeilen: 	0 bis 7
- * 							Spalten: 	0 bis 15
- * Globale Variablen:	matrix[8][2]
- */
+
+
+/**************************************************************************
+* NAME:			printBit
+* Description:		Ändert ein bestimmtes Bit in der LED Matrix.
+* 				Adressierung:
+* 					Zeilen: 	0 bis 7
+* 					Spalten: 	0 bis 15
+*
+* Subroutines Called:	TWI_sendByte();
+*
+* Returns:		keine
+*
+* Globals:		matrix[8][2]
+*
+* Programmer(s):	Michel, Marco, Michael, Christian, Tobias
+* Tested By: Date:
+
+* NOTES:		-
+*
+* REVISION HISTORY
+* Date: By: Description:
+*
+**************************************************************************/
 void printBit(uint8_t ui_row, uint8_t ui_column, uint8_t ui_ledState) {
 	switch(ui_ledState) {
 	case LED_ON:
@@ -45,11 +72,26 @@ void printBit(uint8_t ui_row, uint8_t ui_column, uint8_t ui_ledState) {
 
 }
 
-/*
- * Funktion:			printArray
- * Beschreibung:		Gibt ein ganzes Array vom Format 8 Zeilen x 2 Spalten x 8 Bit auf den LEDs aus.
- * Globale Variablen:	matrix
- */
+
+ /**************************************************************************
+* NAME:			printArray
+* Description:		Gibt ein ganzes Array vom Format 8 Zeilen x 2 Spalten x 8 Bit auf den LEDs aus.
+*
+* Subroutines Called:	TWI_sendArray();
+*
+* Returns:		keine
+*
+* Globals:		matrix[8][2]
+*
+* Programmer(s):	Michel, Marco, Michael, Christian, Tobias
+* Tested By: Date:
+
+* NOTES:		-
+*
+* REVISION HISTORY
+* Date: By: Description:
+*
+**************************************************************************/
 void printArray(uint8_t ui_matrix[8][2]) {
 	for(short row = 0; row < 8; row++) {
 		for(short column = 0; column < 2; column++) {
@@ -59,10 +101,26 @@ void printArray(uint8_t ui_matrix[8][2]) {
 	TWI_sendArray(matrix);
 }
 
-/*
- * Funktion:		ui_getBit
- * Beschreibung:	Diese Funktion gibt den Werte des Bits an der gefragten Stelle in der matrix wieder.
- */
+
+ /**************************************************************************
+* NAME:			ui_getBit
+* Description:		Diese Funktion gibt den Werte des Bits an der gefragten Stelle in der matrix wieder.
+*
+* Subroutines Called:	keine
+*
+* Returns:		uint8_t - Bit der stelle [row][column]
+*
+* Globals:		matrix[8][2]
+*
+* Programmer(s):	Michel, Marco, Michael, Christian, Tobias
+* Tested By: Date:
+
+* NOTES:		-
+*
+* REVISION HISTORY
+* Date: By: Description:
+*
+**************************************************************************/
 uint8_t ui_getBit(uint8_t ui_row, uint8_t ui_column) {
 	if(ui_column > 7) {	//
 		return matrix[ui_row][1] & (1 << (ui_column-8));
@@ -71,6 +129,26 @@ uint8_t ui_getBit(uint8_t ui_row, uint8_t ui_column) {
 	}
 }
 
+
+ /**************************************************************************
+* NAME:			main
+* Description:		????? Wir haben schon eine main	?????
+*
+* Subroutines Called:	???
+*
+* Returns:		int = 0;
+*
+* Globals:		???
+*
+* Programmer(s):	Michel, Marco, Michael, Christian, Tobias
+* Tested By: Date:
+
+* NOTES:		-
+*
+* REVISION HISTORY
+* Date: By: Description:
+*
+**************************************************************************/
 int main() {
 	return 0;
 }

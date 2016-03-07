10f79cb7b2555d08dfaab3701f564201df7d1c7f
tobmag <tobias.mages@gmx.de>
2016-03-07 09:25:24 +0100

    Umschreiben der Kommentare
    Das ist der Kommentaraufbau den Jürgen laut seiner Präsentation(5) wollte.
    1. Dateikopf
    2. Funktionsköpfe

--- AdaVRuit/ATmega32U4.c
+++ AdaVRuit/ATmega32U4.c
@@ -1,26 +1,114 @@
-/*
- * ATmega32U4.c
- *
- *  Created on: Feb 11, 2016
- *      Author: Marco, Michael, Michel, Christian, Tobias
- */
+ /*****************************************************************************
+*
+* FILE: 		ATmega32U4.c
+* PROJECT:		Spielekonsole
+* MODULE:		???
+*
+* Description:		???
+*
+* Notes:		-
+*
+* Compiler dependencies or special instructions:
+*
+* REVISION HISTORY
+* Date: 07.03.2016
+* BY:	Michel, Marco, Michael, Christian, Tobias
+*
+*****************************************************************************/
 
 #include "ATmega32U4.h"
 
+/**************************************************************************
+* NAME:			initATMega32
+* Description:		Initialisierung der Ein- und Ausgänge
+*
+* Subroutines Called:	keine
+*
+* Returns:		keine
+*
+* Globals:		keine
+*
+* Programmer(s):	Michel, Marco, Michael, Christian, Tobias
+* Tested By: Date:
+*
+* NOTES:		-
+*
+* REVISION HISTORY
+* Date: By: Description:
+*
+**************************************************************************/
 void initATMega32() {
 	/* Taster */
     DDRB = 0b10110001;       // Eingänge PortB 1 3 2 6
     DDRF = 0b00001111;       // Eingänge PortF 4 5 6 7
 }
 
+
+/**************************************************************************
+* NAME:			initTWI
+* Description:		Initialisierung des TWI (I2C)
+*
+* Subroutines Called:	???
+*
+* Returns:		keine
+*
+* Globals:		???
+*
+* Programmer(s):	Michel, Marco, Michael, Christian, Tobias
+* Tested By: Date:
+*
+* NOTES:		-
+*
+* REVISION HISTORY
+* Date: By: Description:
+*
+**************************************************************************/
 void initTWI() {
 
 }
 
+/**************************************************************************
+* NAME:			TWI_sendByte
+* Description:		???
+*
+* Subroutines Called:	???
+*
+* Returns:		keine
+*
+* Globals:		???
+*
+* Programmer(s):	Michel, Marco, Michael, Christian, Tobias
+* Tested By: Date:
+*
+* NOTES:		-
+*
+* REVISION HISTORY
+* Date: By: Description:
+*
+**************************************************************************/
 void TWI_sendByte(uint8_t ui_adress, uint8_t ui_data) {
 
 }
 
+/**************************************************************************
+* NAME:			TWI_sendArray
+* Description:		???
+*
+* Subroutines Called:	???
+*
+* Returns:		keine
+*
+* Globals:		???
+*
+* Programmer(s):	Michel, Marco, Michael, Christian, Tobias
+* Tested By: Date:
+*
+* NOTES:		-
+*
+* REVISION HISTORY
+* Date: By: Description:
+*
+**************************************************************************/
 void TWI_sendArray(uint8_t ui_data[8][2]) {
 
 }
@@ -44,35 +132,50 @@ void resetPlayer2_R(uint8_t *_ui_buttons) {(*_ui_buttons &= ~BIT2);}
 void resetPlayer2_U(uint8_t *_ui_buttons) {(*_ui_buttons &= ~BIT1);}
 void resetPlayer2_D(uint8_t *_ui_buttons) {(*_ui_buttons &= ~BIT0);}
 
-/* Vergabe der  Pins:
-        Spieler 1:  Richtung - Port - Pin (Anschluss)
-                    Left     - PF4  - A3
-                    Right    - PF5  - A2
-                    Up       - PF6  - A1
-                    Down     - PF7  - A0
-                    (A6 bis A9 sind als analoge Eing�nge verf�gbar)
-        Spieler 2:
-                    Left     - PB1  - 15
-                    Right    - PB3  - 14
-                    Up       - PB2  - 16
-                    Down     - PB6  - 10
-    Die Pins liegen alle in genau dieser Reihenfolge untereinander
-*/
 
 
 
-/*
- * Beschreibung:       	Auslesen der  Eingabetasten (PortB und  PortF).
- *                     	Man geht davon aus, dass die Tasten AKTIVE LOW sind.
- *                     	Es wurden Makros definiert, um direkt die Richtungen abzufragen
- * Aufbau der Variable: uint8_t ui_buttons;
- *                      	Bit 0-3 -> player 1
- *                     		Bit 4-7 -> player 2
- *                         		Bit 0/4 -> Left
- *                         		Bit 1/5 -> Right
- *                         		Bit 2/6 -> Up
- *                         		Bit 3/7 -> Down
- */
+/**************************************************************************
+* NAME:			ui_eingabe
+* Description:		Auslesen der  Eingabetasten (PortB und  PortF).
+*                     	Man geht davon aus, dass die Tasten AKTIVE LOW sind.
+*                     	Es wurden Funktionen definiert, um direkt die Richtungen abzufragen
+*
+* Subroutines Called:	keine
+*
+* Returns:		uint8_t ui_buttons;
+*                      	Bit 0-3 -> player 1
+*                     		Bit 4-7 -> player 2
+*                         	Bit 0/4 -> Left
+*                         	Bit 1/5 -> Right
+*                         	Bit 2/6 -> Up
+*                         	Bit 3/7 -> Down
+*
+* Globals:		keine
+*
+* Programmer(s):	Michel, Marco, Michael, Christian, Tobias
+* Tested By: Date:
+*
+* NOTES:	
+* 	Vergabe der  Pins:
+*        Spieler 1:  Richtung - Port - Pin (Anschluss)
+*                    Left     - PF4  - A3
+*                    Right    - PF5  - A2
+*                    Up       - PF6  - A1
+*                    Down     - PF7  - A0
+*                    (A6 bis A9 sind als analoge Eing�nge verf�gbar)
+*        Spieler 2:
+*                    Left     - PB1  - 15
+*                    Right    - PB3  - 14
+*                    Up       - PB2  - 16
+*                    Down     - PB6  - 10
+*    	Die Pins liegen alle in dieser Reihenfolge untereinander
+*
+*
+* REVISION HISTORY
+* Date: By: Description:
+*
+**************************************************************************/
 uint8_t ui_eingabe(){
     uint8_t ui_bufferPF = 0x00, ui_bufferPB = 0x00, ui_buttons = 0x00;
     ui_bufferPF = ~PINF;
@@ -91,13 +194,50 @@ uint8_t ui_eingabe(){
 	return ui_buttons;
 }
 
-/*
- * Funktion:
- * Beschreibung:		Die Funktionen fragen beim Aufruf die zugehörigen Pins ab.
- * 						Sie geben eine 0x01 zurück, falls die Taste gedrückt ist. ansonten = 0x00
- * 						Es findet bisher kein Entprellen statt.
- * Globale Variablen:	Keine
- */
+
+ /**************************************************************************
+* NAME:			ui_eingabe
+* Description:		Die Funktionen fragen beim Aufruf die zugehörigen Pins ab.
+* 			Sie geben eine 0x01 zurück, falls die Taste gedrückt ist. ansonten = 0x00
+* 			Es findet bisher kein Entprellen statt.
+* 			Auslesen der  Eingabetasten (PortB und  PortF).
+*                     	Man geht davon aus, dass die Tasten AKTIVE LOW sind.
+*
+* Subroutines Called:	keine
+*
+* Returns:		uint8_t ui_buttons;
+*                      	Bit 0-3 -> player 1
+*                     		Bit 4-7 -> player 2
+*                         	Bit 0/4 -> Left
+*                         	Bit 1/5 -> Right
+*                         	Bit 2/6 -> Up
+*                         	Bit 3/7 -> Down
+*
+* Globals:		keine
+*
+* Programmer(s):	Michel, Marco, Michael, Christian, Tobias
+* Tested By: Date:
+*
+* NOTES:	
+* 	Vergabe der  Pins:
+*        Spieler 1:  Richtung - Port - Pin (Anschluss)
+*                    Left     - PF4  - A3
+*                    Right    - PF5  - A2
+*                    Up       - PF6  - A1
+*                    Down     - PF7  - A0
+*                    (A6 bis A9 sind als analoge Eing�nge verf�gbar)
+*        Spieler 2:
+*                    Left     - PB1  - 15
+*                    Right    - PB3  - 14
+*                    Up       - PB2  - 16
+*                    Down     - PB6  - 10
+*    	Die Pins liegen alle in dieser Reihenfolge untereinander
+*
+*
+* REVISION HISTORY
+* Date: By: Description:
+*
+**************************************************************************/
 uint8_t ui_player1_L() {
     	uint8_t ui_bufferPF = 0x00;
     	ui_bufferPF = ~PINF;

18d97ef1b18cd11328429b050c0f6e4e97b0e1f4
lowmotion <marcojedzig@gmail.com>
2016-03-12 22:43:21 +0100

    Menü nun vorhanden (Einbindungen fehlen noch)
    1. Pong ist verknüpft und ausführbar.

---
+++ AdaVRuit/menu.h
@@ -0,0 +1,15 @@
+#ifndef MENU_H_
+#define MENU_H_
+
+#include "main.h"
+
+#include <util/delay.h>
+
+/* Konstanten */
+#define MENU_ENTRIES 3
+
+/* Funktionen */
+
+void menu();
+
+#endif /* MENU_H_*/

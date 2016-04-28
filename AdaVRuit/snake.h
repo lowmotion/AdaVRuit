8e75dc11fb3a6ecf8e690eb7d08000550c5a2f80
karmicha <michael.karp@topmail-files.de>
2016-04-28 11:05:19 +0200

    Kommentarkoepfe korrigiert

--- AdaVRuit/snake.h
+++ AdaVRuit/snake.h
@@ -1,12 +1,30 @@
+/***************************************************************************** 
+* 
+* Datei: 		snake.h 
+* Kurs:			TEN14 
+* Projekt:		Spielekonsole 
+* Modul:		Zentrale Einheit 
+* 
+* Beschreibung:	Diese Datei enthält die Hauptroutine und alle Funktionen des Spiels "Snake"
+* 
+* Autor:		Michel Denniger, Marco Jedzig, Michael Karp, Tobias Mages, Christian Wagner 
+* 				 
+* 
+* Datum: 		28.04.2016 
+* 
+*****************************************************************************/ 
 #ifndef _SNAKE_H_
 #define _SNAKE_H_
 
+/*************** 
+* INCLUDES 
+***************/
 #include "main.h"
 
 #include <avr/interrupt.h>
 
 /***********************
- * Defines und Makros
+ * DEFINES UND MAKROS
  ***********************/
 //Richtungen der Schlange
 #define UP		0
@@ -21,8 +39,28 @@
 #define MAX_LENGTH 20
 
 /****************
- * Funktionen
+ * FUNKTIONEN
  ****************/
+ 
+/*****************************************************************************  
+* Name:			playSnake  
+* Beschreibung:		Hauptroutine des Spiels Snake  
+*  
+* Subroutinen:		_delay_ms() aus <util/delay.h>	  
+* 			initSnake() 
+*       random()
+*       PartOfSnake()
+*       makeFood()
+* 			ui_input() aus AdaVRuit.h  
+* 			checkButtons()  
+* 			nextstep()  
+* 			drawSnake()  
+* 			  
+* Rückgabewert:		keine  
+*  
+* Globale Var.:		int8_t ui_timerFlag:  aus AdaVRuit.h  
+*   
+******************************************************************************/  
 void playSnake();
 
 #endif
